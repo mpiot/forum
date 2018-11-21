e4eed4435077605ebc90a41b459a435ce759c791
Mathieu Piot <math.piot@gmail.com>
2018-11-21 16:32:09 +0100

    PSR fix

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,13 +1,26 @@
 <?php
 
+/*
+ * Copyright 2018 Mathieu Piot.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace App\Controller;
 
 use App\Entity\Category;
-use App\Form\CategoryType;
-use App\Repository\CategoryRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
