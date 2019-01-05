d061fc9d4b84c04c1c5ede5f477143941ace16ea
Mathieu Piot <math.piot@gmail.com>
2019-01-05 13:30:55 +0100

    Fix travis-ci

--- docker/scripts/deploy-dev.sh
+++ docker/scripts/deploy-dev.sh
@@ -3,7 +3,7 @@
 SHA_APP_IMAGE_DEV_TAG=`docker images -q ${APP_IMAGE_NAME}:$DEV_TAG`
 SHA_APP_IMAGE_CI=`docker images -q ${APP_IMAGE_NAME}:ci`
 
-if [[ "$SHA_APP_IMAGE_DEV_TAG" -ne "$SHA_APP_IMAGE_CI" ]]; then
+if [[ "$SHA_APP_IMAGE_DEV_TAG" != "$SHA_APP_IMAGE_CI" ]]; then
     docker tag ${APP_IMAGE_NAME}:ci $APP_IMAGE_NAME:$DEV_TAG
     docker tag ${APP_IMAGE_NAME}:ci $APP_IMAGE_NAME:dev-latest
 
