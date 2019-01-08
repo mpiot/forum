8b65e97786fd859d19b2a235795632ad859de425
Mathieu Piot <math.piot@gmail.com>
2019-01-08 23:08:16 +0100

    Add user administration

---
+++ src/Controller/UserAdminController.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * Copyright 2018 Mathieu Piot.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/user")
+ * @Security("is_granted('ROLE_ADMIN')")
+ */
+class UserAdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="user_admin_index", methods="GET")
+     * @Entity("users", class="App\Entity\User", expr="repository.findAll()")
+     */
+    public function index(array $users): Response
+    {
+        return $this->render('user_admin/index.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="user_admin_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_admin_index');
+        }
+
+        return $this->render('user_admin/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="user_admin_delete", methods="DELETE")
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($user === $this->getUser()) {
+            $this->addFlash('error', 'You can\'t delete your own account from this place.');
+
+            return $this->redirectToRoute('user_admin_index');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('user_admin_index');
+    }
+}
